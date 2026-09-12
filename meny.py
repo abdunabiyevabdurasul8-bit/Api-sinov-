51bc81c07934ab681ba631dad6c738b58206706a
abdunabiyevabdurasul8-bit <abdunabiyevabdurasul8@gmail.com>
2026-09-12 18:39:36 +0600

    Update meny.py

--- meny.py
+++ meny.py
@@ -9,7 +9,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-TOKEN = ""
+TOKEN = "8799964859:AAHqdOHx_K6L0Ms_VLKqeT12RDRsF0_U7jc"
 ADMIN_ID = 5692925792
 
 bot = Bot(token=TOKEN)
