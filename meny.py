09140154e9961a13e0eb7193ea2e706967cc7deb
abdunabiyevabdurasul8-bit <abdunabiyevabdurasul8@gmail.com>
2026-09-13 11:11:08 +0500

    Update BOT_TOKEN in meny.py
    Replaced default BOT_TOKEN with a specific token.

--- meny.py
+++ meny.py
@@ -27,7 +27,7 @@ from telegram.ext import (
 # SOZLAMALAR
 # =========================================================
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8799964859:AAE1ykyVhycVfPwT9z7X-DbCtMs6A5Kxcl0")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 PORT = int(os.getenv("PORT", "10000"))
